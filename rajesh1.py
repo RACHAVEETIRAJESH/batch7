9420271596d3c4121f9a4f1717d12e2558d5027c
RACHAVEETIRAJESH <162981874+RACHAVEETIRAJESH@users.noreply.github.com>
2024-03-11 16:00:33 +0530

    Add files via upload

---
+++ rajesh1.py
@@ -0,0 +1,101 @@
+
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+'''
+
+----->commenting
+1.)  Commenting is used for making documentation in a python file.
+2.)  When the line is commented,that line will not be considered as a part of the code.
+3.)  When the program is executed,commented line will not be in the part of execution.
+
+
+-----> NOTE:
+Single line commenting can be done using # at the beginning of the line
+
+     Eg:
+         #print("hello world")
+
+
+Multiple line commenting can be done using  code . The lines between the quotes will be commented.
+
+
+Eg:
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+
+     
+         
+#print("hello world")
+   
+
+import this
+
+Bargained Characters
+
+ Controls and data structures ----> ABC language
+ Strings amd its formats ----> from C
+ Regular expressions -----> Pearl
+ Exception handling -----> Module 3
+ Magic methods from----> forton
+# import is a keyword which is used to communicated between the python files.
+ Flavours of python
+-Cpython - written in C, Most common implementation of python.
+-Jython - java implementation of python.
+-IronPython -  implementation of python programming language which is tightly integrated with .NET.
+-RubyPython - Bridge between Python and Ruby
+
+# ----> print(object, sep, end, file, flush)
+#print has 5 parameters
+
+#object ----> the value to be printed is object
+# sep ----> separator
+#Eg
+# a=8
+#b=9
+#c=89
+# print(a,b,c, sep='$')
+
+# end ---> which is used to add the string at the end of the value,
+## it will not keep the command to stay in the same line
+# Eg:
+# print(str(a),b,end ='%%')
+# print(str(c))
+
+# ! -------> variables
+# variables is a name which is used to identify the value of the variable
+# ?which is stored in a specific memory location
+
+# Rules to define the variables
+1.) variable have to start with only alphabets, underscores
+2.) the char following the 1st char can be alphabets,numbers, underscores
+3.) no spaces are allowed
+4.) no special charecters like !@#$%^&*()_+
+5.) case sensitive
+6.) keywords are not allowed as variables
+
+# ! ways to define variables
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import antigravity        
